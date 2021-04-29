54412d84b277e8c83b9043c433a8dba9ef5cc586
Görcs Balázs <gorcs.balazs@gmail.com>
2021-04-29 10:11:15 +0200

    local green tests

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,20 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        if (input == null || input.length() == 0) {
+            return -1;
+        }
+        int sum = 0;
+        for (int i = 0; i < input.length(); i++) {
+            try {
+                int num = Integer.parseInt(String.valueOf(input.charAt(i)));
+                sum += num;
+            } catch (NumberFormatException ex) {
+                
+            }
+        }
+        return sum;
+    }
 }
